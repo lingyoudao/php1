d3b903ea2a1a2fc4c8c66f4360d9ab3a323310be
lingyoudao <lingyoudao@hotmail.com>
2022-09-05 09:32:31 +0800

    lesson4-index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>会员管理系统</title>
+</head>
+<style>
+    .main{width: 80%; margin: 0 auto; text-align: center;}
+    h2{font-size: 20px;}
+    h2 a{text-decoration: none;margin-right: 15px;}
+    h2 a :last-child{margin-right: 0;}
+    h2 a:hover{color:brown; text-decoration: underline;}
+    .current{color:brown;}
+</style>
+<body>
+<div class="main">
+    <h1>
+        会员管理系统
+    </h1>
+    <h2>
+        <a href="index.php" class="current">首页</a>
+        <a href="signup.php">注册</a>
+        <a href="login.php">登陆</a>
+        <a href="modify.php">个人资料</a>
+        <a href="admin.php">后台管理</a>
+    </h2>
+</div>
+</body>
+</html>
