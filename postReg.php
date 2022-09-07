5a28a9c64e4e993c1d3ab1659288e10f0b138241
lingyoudao <lingyoudao@hotmail.com>
2022-09-07 21:32:24 +0800

    10-表单验证

--- postReg.php
+++ postReg.php
@@ -17,11 +17,30 @@ mysqli_query($conn, "set names utf8");
 if(!strlen($username) || !strlen($pw)){
     echo '<script>alert("用户名和密码不能为空");history.back()</script>';
     exit;
+}else{
+    if(!preg_match('/^[a-zA-Z0-9]{3,10}$/', $username)){
+        echo '<script>alert("用户名必填，为字母和数字组合，长度3-10位");history.back()</script>';
+        exit;
+    }
 }
+
 if($pw<>$cpw){
     echo '<script>alert("两次输入的密码不同");history.back()</script>';
     exit;
+}else{
+    if(!preg_match('/^[a-zA-Z0-9_*]{6,10}$/', $pw)){
+        echo '<script>alert("密码格式必填，为字母和数字以及下划线_或*组合，长度3-10位");history.back()</script>';
+        exit;
+    }
 }
+
+if(!empty($email)){
+    if(!preg_match('/^[a-zA-Z0-9_\-]+@([a-zA-Z0-9]+\.)+(com|cn|net|org)$/', $email)){
+        echo '<script>alert("信箱格式不正确!");history.back()</script>';
+        exit;
+    }
+}
+
 //判断是否用户名已经存在
 $sql = "select * from userinfo where username='$username'";
 $result = mysqli_query($conn, $sql);
--- signup.php
+++ signup.php
@@ -13,6 +13,7 @@
     h2 a :last-child{margin-right: 0;}
     h2 a:hover{color:brown; text-decoration: underline;}
     .current{color:brown;}
+    .red{color: red;}
 </style>
 <body>
 <div class="main">
@@ -26,19 +27,19 @@
         <a href="modify.php">个人资料</a>
         <a href="admin.php">后台管理</a>
     </h2>
-    <form action="postReg.php" method="POST">
+    <form action="postReg.php" method="POST" onsubmit="return check()">
         <table align="center" border="1" style="border-collapse: collapse;" cellpadding="10" cellspacing="0">
             <tr>
                 <td align="right">用户名:</td>
-                <td align="left"><input name="username"></td>
+                <td align="left"><input name="username"><span class="red">*</span></td>
             </tr>
             <tr>
                 <td align="right">密码:</td>
-                <td align="left"><input type="password" name="pw"></td>
+                <td align="left"><input type="password" name="pw"><span class="red">*</span></td>
             </tr>
             <tr>
                 <td align="right">确认密码:</td>
-                <td align="left"><input type="password" name="cpw"></td>
+                <td align="left"><input type="password" name="cpw"><span class="red">*</span></td>
             </tr>
             <tr>
                 <td align="right">邮箱:</td>
@@ -70,5 +71,42 @@
         </table>
     </form>
 </div>
+<script>
+    function check(){
+        let username = document.getElementsByName("username")[0].value.trim();
+        let pw = document.getElementsByName("pw")[0].value.trim();
+        let cpw = document.getElementsByName("cpw")[0].value.trim();
+        let email = document.getElementsByName("email")[0].value.trim();
+
+        let nameReg = /^[a-zA-Z0-9]{3,10}$/ //正则表达式中以/^$/标记开始和结束,长度3-10位
+        if(!nameReg.test(username))
+        {
+            alert("用户名必填，为字母和数字组合，长度3-10位");
+            return false;
+        }
+
+        let pwReg = /^[a-zA-Z0-9_*]{6,10}$/ //包括下划线_和*
+        if(!pwReg.test(pw)){
+            alert("密码格式必填，为字母和数字以及下划线_或*组合，长度3-10位");
+            return false;
+        }
+
+        if(pw!=cpw){
+            alert("密码和确认密码必须相同");
+            return false;
+        }
+
+        let emailReg = /^[a-zA-Z0-9_\-]+@([a-zA-Z0-9]+\.)+(com|cn|net|org)$/
+        if(email.length>0){
+            if(emailReg.test(email)){
+                alert('信箱格式不正确!');
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+</script>
 </body>
 </html>
